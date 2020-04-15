2b7941d30215d8275fc321e73d4b0f02bd8c8dd4
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2020-04-15 21:11:06 +0700

    updated color + splash

--- app/src/main/java/com/doancnpm/edoctor/SplashActivity.kt
+++ app/src/main/java/com/doancnpm/edoctor/SplashActivity.kt
@@ -11,7 +11,6 @@ class SplashActivity : BaseActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_splash)
 
     vm.authEvent.observeEvent(this) {
       if (it) {
