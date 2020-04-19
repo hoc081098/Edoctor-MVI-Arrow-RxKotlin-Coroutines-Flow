10a8b87b647f29e12b37f9eaa6536f31e599b88d
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2020-04-20 00:47:36 +0700

    fix nav auth

--- app/src/main/java/com/doancnpm/edoctor/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/doancnpm/edoctor/ui/auth/AuthActivity.kt
@@ -1,15 +1,34 @@
 package com.doancnpm.edoctor.ui.auth
 
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.doancnpm.edoctor.R
 import com.doancnpm.edoctor.core.BaseActivity
 import com.doancnpm.edoctor.databinding.ActivityAuthBinding
 import kotlin.LazyThreadSafetyMode.NONE
 
 class AuthActivity : BaseActivity() {
   private val binding by lazy(NONE) { ActivityAuthBinding.inflate(layoutInflater) }
+  private lateinit var appBarConfiguration: AppBarConfiguration
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(binding.root)
+
+    val navHostFragment =
+      supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    val navController = navHostFragment.navController
+    appBarConfiguration = AppBarConfiguration(navController.graph)
+    setupActionBarWithNavController(
+      navController,
+      appBarConfiguration
+    )
   }
+
+  override fun onSupportNavigateUp(): Boolean =
+    findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
 }
