3029cb5e22e60f6464a60db3ad0c594de4d3d55b
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2020-05-20 12:44:04 +0700

    fix done

--- app/src/main/java/com/doancnpm/edoctor/data/local/UserLocalSourceImpl.kt
+++ app/src/main/java/com/doancnpm/edoctor/data/local/UserLocalSourceImpl.kt
@@ -1,75 +1,55 @@
 package com.doancnpm.edoctor.data.local
 
 import android.content.SharedPreferences
-import androidx.core.content.edit
-import arrow.core.toOption
 import com.doancnpm.edoctor.data.local.model.UserLocal
 import com.doancnpm.edoctor.data.local.model.UserLocalJsonAdapter
 import com.doancnpm.edoctor.domain.dispatchers.AppDispatchers
+import com.doancnpm.edoctor.utils.delegate
+import com.doancnpm.edoctor.utils.observe
 import kotlinx.coroutines.withContext
 
 class UserLocalSourceImpl(
-  rxkPrefs: RxkPrefs,
-  private val sharedPreferences: SharedPreferences,
+  sharedPreferences: SharedPreferences,
   private val dispatchers: AppDispatchers,
   private val userLocalJsonAdapter: UserLocalJsonAdapter,
 ) : UserLocalSource {
 
-  private val tokenPref = rxkPrefs.string(TOKEN_KEY)
-  private val userPref = rxkPrefs.string(USER_KEY)
+  private var token by sharedPreferences.delegate(null as String?, TOKEN_KEY, commit = true)
+  private var userLocal by sharedPreferences.delegate(null as String?, USER_KEY, commit = true)
 
-  private val tokenObservable = tokenPref.observe()
-    .map { it.nullIfBlank().toOption() }
+  private val tokenObservable = sharedPreferences.observe(null as String?, TOKEN_KEY)
     .replay(1)
-    .refCount()
+    .refCount()!!
 
-  private val userObservable = userPref.observe()
-    .map { it.toUserLocal().toOption() }
+  private val userObservable = sharedPreferences.observe(null as String?, USER_KEY)
+    .map { json -> json.mapNotNull { it.toUserLocal() } }
     .replay(1)
-    .refCount()
+    .refCount()!!
 
-  override fun token() = tokenPref.get().nullIfBlank()
+  override fun token() = token
 
-  override suspend fun saveToken(token: String) {
-    withContext(dispatchers.io) {
-      sharedPreferences.edit(commit = true) {
-        putString(TOKEN_KEY, token)
-      }
-    }
-  }
+  override suspend fun saveToken(token: String) =
+    withContext(dispatchers.io) { this@UserLocalSourceImpl.token = token }
 
   override fun tokenObservable() = tokenObservable
 
-  override suspend fun saveUser(user: UserLocal) {
-    withContext(dispatchers.io) {
-      sharedPreferences.edit(commit = true) {
-        putString(USER_KEY, userLocalJsonAdapter.toJson(user))
-      }
-    }
+  override suspend fun saveUser(user: UserLocal) = withContext(dispatchers.io) {
+    this@UserLocalSourceImpl.userLocal = userLocalJsonAdapter.toJson(user)
   }
 
-  override suspend fun user(): UserLocal? {
-    return withContext(dispatchers.io) { userPref.get().toUserLocal() }
-  }
+  override suspend fun user() = withContext(dispatchers.io) { userLocal.toUserLocal() }
 
   override fun userObservable() = userObservable
 
-  override suspend fun removeUserAndToken() {
-    withContext(dispatchers.io) {
-      sharedPreferences.edit(commit = true) {
-        putString(TOKEN_KEY, "")
-        putString(USER_KEY, "")
-      }
-    }
+  override suspend fun removeUserAndToken() = withContext(dispatchers.io) {
+    userLocal = null
+    token = null
   }
 
-  private fun String.toUserLocal() =
-    kotlin.runCatching { userLocalJsonAdapter.fromJson(this) }
-      .getOrNull()
+  private fun String?.toUserLocal(): UserLocal? =
+    runCatching { userLocalJsonAdapter.fromJson(this ?: return null) }.getOrNull()
 
   private companion object {
-    fun String.nullIfBlank() = if (this.isBlank()) null else this
-
     const val TOKEN_KEY = "com.doancnpm.edoctor.token"
     const val USER_KEY = "com.doancnpm.edoctor.user"
   }
