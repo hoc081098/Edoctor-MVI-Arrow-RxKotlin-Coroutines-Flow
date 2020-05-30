68cba4ccd4ed092d8771a2b98ae4eb88dc4b2518
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2020-05-30 15:27:08 +0700

    feat(main)

--- app/src/main/java/com/doancnpm/edoctor/ui/main/history/HistoryFragment.kt
+++ app/src/main/java/com/doancnpm/edoctor/ui/main/history/HistoryFragment.kt
@@ -1,14 +1,14 @@
-package com.doancnpm.edoctor.ui.main.home
+package com.doancnpm.edoctor.ui.main.history
 
 import android.os.Bundle
 import android.view.View
 import com.doancnpm.edoctor.R
 import com.doancnpm.edoctor.core.BaseFragment
-import com.doancnpm.edoctor.databinding.FragmentHomeBinding
+import com.doancnpm.edoctor.databinding.FragmentHistoryBinding
 import com.doancnpm.edoctor.utils.viewBinding
 
-class HomeFragment : BaseFragment(R.layout.fragment_home) {
-  private val binding by viewBinding { FragmentHomeBinding.bind(it) }
+class HistoryFragment : BaseFragment(R.layout.fragment_history) {
+  private val binding by viewBinding { FragmentHistoryBinding.bind(it) }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/doancnpm/edoctor/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/doancnpm/edoctor/ui/main/profile/ProfileFragment.kt
@@ -1,14 +1,14 @@
-package com.doancnpm.edoctor.ui.main.home
+package com.doancnpm.edoctor.ui.main.profile
 
 import android.os.Bundle
 import android.view.View
 import com.doancnpm.edoctor.R
 import com.doancnpm.edoctor.core.BaseFragment
-import com.doancnpm.edoctor.databinding.FragmentHomeBinding
+import com.doancnpm.edoctor.databinding.FragmentProfileBinding
 import com.doancnpm.edoctor.utils.viewBinding
 
-class HomeFragment : BaseFragment(R.layout.fragment_home) {
-  private val binding by viewBinding { FragmentHomeBinding.bind(it) }
+class ProfileFragment : BaseFragment(R.layout.fragment_profile) {
+  private val binding by viewBinding { FragmentProfileBinding.bind(it) }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
