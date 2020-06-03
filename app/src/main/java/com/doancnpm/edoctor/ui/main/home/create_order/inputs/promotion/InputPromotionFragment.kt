7a170046610817efc5540a069a6cbcd1ab37dcb1
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2020-06-03 19:21:36 +0700

    feat(create-order): init

--- app/src/main/java/com/doancnpm/edoctor/ui/main/home/create_order/inputs/promotion/InputPromotionFragment.kt
+++ app/src/main/java/com/doancnpm/edoctor/ui/main/home/create_order/inputs/promotion/InputPromotionFragment.kt
@@ -5,13 +5,18 @@ import android.view.View
 import com.doancnpm.edoctor.R
 import com.doancnpm.edoctor.core.BaseFragment
 import com.doancnpm.edoctor.databinding.FragmentInputPromotionBinding
+import com.doancnpm.edoctor.ui.main.home.create_order.CreateOrderVM
 import com.doancnpm.edoctor.utils.viewBinding
+import org.koin.androidx.viewmodel.ext.android.getViewModel
+import kotlin.LazyThreadSafetyMode.NONE
 
 class InputPromotionFragment : BaseFragment(R.layout.fragment_input_promotion) {
   private val binding by viewBinding<FragmentInputPromotionBinding>()
+  private val viewModel by lazy(NONE) { requireParentFragment().getViewModel<CreateOrderVM>() }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-    binding.root
+    binding
+    viewModel
   }
 }
