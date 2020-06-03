513f8067e30ff9a40b661fd4499b0db54ec01724
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2020-06-03 16:25:22 +0700

    feat(create-order): init

--- app/src/main/java/com/doancnpm/edoctor/ui/main/home/create_order/CreateOrderFragment.kt
+++ app/src/main/java/com/doancnpm/edoctor/ui/main/home/create_order/CreateOrderFragment.kt
@@ -2,7 +2,9 @@ package com.doancnpm.edoctor.ui.main.home.create_order
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.doancnpm.edoctor.R
@@ -12,6 +14,9 @@ import com.doancnpm.edoctor.ui.main.home.create_order.inputs.address.InputAddres
 import com.doancnpm.edoctor.ui.main.home.create_order.inputs.promotion.InputPromotionFragment
 import com.doancnpm.edoctor.ui.main.home.create_order.inputs.time.InputTimeFragment
 import com.doancnpm.edoctor.utils.viewBinding
+import com.jakewharton.rxbinding4.viewpager2.pageSelections
+import io.reactivex.rxjava3.kotlin.addTo
+import io.reactivex.rxjava3.kotlin.subscribeBy
 import kotlin.LazyThreadSafetyMode.NONE
 
 class CreateOrderFragment : BaseFragment(R.layout.fragment_create_order) {
@@ -27,13 +32,53 @@ class CreateOrderFragment : BaseFragment(R.layout.fragment_create_order) {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+
+    requireActivity()
+      .onBackPressedDispatcher
+      .addCallback(owner = viewLifecycleOwner) {
+        if (binding.viewPager.currentItem - 1 in fragments.indices) {
+          binding.viewPager.currentItem--
+        } else {
+          findNavController().popBackStack()
+        }
+      }
     setupViews()
   }
 
   private fun setupViews() {
-    binding.viewPager.run {
-      adapter = CreateOrderViewPagerAdapter(this@CreateOrderFragment, fragments)
+    val viewPager = binding.viewPager
+
+    viewPager.adapter = CreateOrderViewPagerAdapter(this@CreateOrderFragment, fragments)
+    viewPager
+      .pageSelections()
+      .subscribeBy {
+        when (it) {
+          0 -> binding.prevButton.isEnabled = false
+          fragments.lastIndex -> binding.nextButton.isEnabled = false
+          else -> {
+            binding.prevButton.isEnabled = true
+            binding.nextButton.isEnabled = true
+          }
+        }
+      }
+      .addTo(compositeDisposable)
+
+    val onClickListener = View.OnClickListener {
+      when (it) {
+        binding.nextButton -> {
+          if (viewPager.currentItem + 1 in fragments.indices) {
+            viewPager.currentItem++
+          }
+        }
+        binding.prevButton -> {
+          if (viewPager.currentItem - 1 in fragments.indices) {
+            viewPager.currentItem--
+          }
+        }
+      }
     }
+    binding.nextButton.setOnClickListener(onClickListener)
+    binding.prevButton.setOnClickListener(onClickListener)
   }
 }
 
