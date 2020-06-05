12799ced8871db72db915c10f22d4daaee807819
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2020-06-05 15:08:56 +0700

    feat(create-order): init location

--- app/src/main/java/com/doancnpm/edoctor/ui/main/home/create_order/CreateOrderVM.kt
+++ app/src/main/java/com/doancnpm/edoctor/ui/main/home/create_order/CreateOrderVM.kt
@@ -1,13 +1,50 @@
 package com.doancnpm.edoctor.ui.main.home.create_order
 
+import android.annotation.SuppressLint
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.map
+import androidx.lifecycle.viewModelScope
 import com.doancnpm.edoctor.core.BaseVM
 import com.doancnpm.edoctor.utils.asLiveData
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import timber.log.Timber
+import kotlin.LazyThreadSafetyMode.NONE
 
-class CreateOrderVM : BaseVM() {
+data class Location(val lat: Double, val lng: Double)
 
-  val canGoNextLiveDatas = List(4) {
-    MutableLiveData<Boolean>().apply { value = true }.asLiveData()
+class CreateOrderVM(private val application: Application) : BaseVM() {
+  private val fusedLocationClient by lazy(NONE) {
+    LocationServices.getFusedLocationProviderClient(application)
   }
+
+  private val locationD = MutableLiveData<Location>()
+
+  val canGoNextLiveDatas = listOf(
+    locationD.map { it !== null },
+    locationD.map { it !== null },
+    locationD.map { it !== null },
+    locationD.map { it !== null },
+  )
+  val locationLiveData get() = locationD.asLiveData()
+
+  @SuppressLint("MissingPermission")
+  fun getCurrentLocation() {
+    viewModelScope.launch {
+      fusedLocationClient.lastLocation
+        .await()
+        .also { Timber.d("lastLocation: $it") }
+        ?.let {
+          locationD.value = Location(
+            lat = it.latitude,
+            lng = it.longitude
+          )
+          return@launch
+        }
+    }
+  }
+
 
 }
