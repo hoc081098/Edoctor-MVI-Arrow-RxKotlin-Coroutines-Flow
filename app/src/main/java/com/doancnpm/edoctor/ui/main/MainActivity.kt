70b87dec436f8b309846144e6be7e6179968752d
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2020-06-14 23:11:18 +0700

    alert when exiting

--- app/src/main/java/com/doancnpm/edoctor/ui/main/MainActivity.kt
+++ app/src/main/java/com/doancnpm/edoctor/ui/main/MainActivity.kt
@@ -24,6 +24,8 @@ class MainActivity : BaseActivity() {
   private val binding by viewBinding(ActivityMainBinding::inflate)
   private val viewModel by viewModel<MainVM>()
 
+  var onSupportNavigateUp: (() -> Boolean)? = null
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(binding.root)
@@ -81,6 +83,7 @@ class MainActivity : BaseActivity() {
   }
 
   override fun onSupportNavigateUp(): Boolean {
+    onSupportNavigateUp?.invoke()?.let { return it }
     return currentNavController?.value?.navigateUp() ?: false
   }
 
