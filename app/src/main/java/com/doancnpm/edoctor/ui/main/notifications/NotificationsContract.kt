5831ab786ebd152dde6731f6ad5ffddaa3c269c1
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2020-06-15 21:45:15 +0700

    fix(notifications): wrong empty state 🎉🎉🎉🎉

--- app/src/main/java/com/doancnpm/edoctor/ui/main/notifications/NotificationsContract.kt
+++ app/src/main/java/com/doancnpm/edoctor/ui/main/notifications/NotificationsContract.kt
@@ -4,8 +4,9 @@ import com.doancnpm.edoctor.domain.entity.AppError
 
 interface NotificationsContract {
   sealed class PlaceholderState {
-    data class Idle(val isInitial: Boolean = true) : PlaceholderState()
+    object Idle : PlaceholderState()
     object Loading : PlaceholderState()
+    data class Success(val isEmpty: Boolean) : PlaceholderState()
     data class Error(val error: AppError) : PlaceholderState()
   }
 }
