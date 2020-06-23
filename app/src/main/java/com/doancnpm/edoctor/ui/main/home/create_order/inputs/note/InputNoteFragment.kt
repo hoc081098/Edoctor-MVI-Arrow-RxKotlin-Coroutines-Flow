a29b63611bb5c1b8cc91ca59708fbfd59a952c20
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2020-06-23 15:56:23 +0700

    fix: input note

--- app/src/main/java/com/doancnpm/edoctor/ui/main/home/create_order/inputs/note/InputNoteFragment.kt
+++ app/src/main/java/com/doancnpm/edoctor/ui/main/home/create_order/inputs/note/InputNoteFragment.kt
@@ -1,20 +1,19 @@
 package com.doancnpm.edoctor.ui.main.home.create_order.inputs.note
 
 import android.os.Bundle
-import android.view.KeyEvent
 import android.view.View
-import android.view.inputmethod.EditorInfo
 import com.doancnpm.edoctor.R
 import com.doancnpm.edoctor.core.BaseFragment
 import com.doancnpm.edoctor.databinding.FragmentInputNoteBinding
+import com.doancnpm.edoctor.ui.main.MainActivity
 import com.doancnpm.edoctor.ui.main.home.create_order.CreateOrderVM
-import com.doancnpm.edoctor.utils.hideKeyboard
 import com.doancnpm.edoctor.utils.viewBinding
 import com.jakewharton.rxbinding4.widget.textChanges
 import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.kotlin.subscribeBy
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEventListener
 import org.koin.androidx.viewmodel.ext.android.getViewModel
-import timber.log.Timber
 import kotlin.LazyThreadSafetyMode.NONE
 
 class InputNoteFragment : BaseFragment(R.layout.fragment_input_note) {
@@ -23,6 +22,21 @@ class InputNoteFragment : BaseFragment(R.layout.fragment_input_note) {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+
+    KeyboardVisibilityEvent.setEventListener(
+      requireActivity(),
+      viewLifecycleOwner,
+      object : KeyboardVisibilityEventListener {
+        override fun onVisibilityChanged(isOpen: Boolean) {
+          val mainActivity = requireActivity() as MainActivity
+          if (isOpen) {
+            mainActivity.hideBottomNav()
+          } else {
+            mainActivity.showBottomNav()
+          }
+        }
+      }
+    )
 
     viewModel.noteLiveData
       .value
